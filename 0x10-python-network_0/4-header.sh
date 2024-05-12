3ac57fc6a23259e47bb80b246db6f516d2987d74
Abel Eyasu <abeleyasu98@gmail.com>
2024-05-11 23:10:15 -0700

    task 4 correction

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # This Bash script takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-# A header variable X-School-User-Id must be sent with the value 98
-curl -sH "X-School-User-Id:98" "$1"
-
+# A header variable X-School-User-Id is sent with the value 98
+curl -s -X GET -H "X-School-User-Id: 98" "$1"
