67bf4cbd0ccc6c8c9f64bd4b2e1a70d3584ac042
abeleyasu <abeleyasu98@gmial.com>
2024-06-28 07:48:46 -0700

    task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,7 @@
+-- List all privileges of user_0d_1 and user_0d_2 on localhost
+
+-- Listing privileges for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Listing privileges for user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
--- 0x0E-SQL_more_queries/5-unique_id.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE IF NOT EXISTS unique_id
-       (id INT DEFAULT 1,
-       UNIQUE (ID),
-       name VARCHAR(256));
