ff0b46a9d87d4954195f2ee566fe88c9e27e3dea
abeleyasu <abeleyasu98@gmail.com>
2024-08-02 11:44:05 -0700

    103-max_state.sql

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,9 +1,6 @@
--- Import the provided table dump into the hbtn_0c_0 database
-USE hbtn_0c_0;
-SOURCE '/home/user/Desktop/Download/cities.sql';
-
--- Calculate the average temperature by city and order the result by temperature (descending)
-SELECT city, AVG(temperature) AS avg_temp
-FROM cities
-GROUP BY city
-ORDER BY avg_temp DESC;
+-- Displays the average temperature (in Fahrenheit)
+-- by city ordered by descending temperature.
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+GROUP BY `city`
+ORDER BY `avg_temp` DESC;
