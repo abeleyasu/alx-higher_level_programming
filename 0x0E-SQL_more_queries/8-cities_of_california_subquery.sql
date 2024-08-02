adeb39ce93fb29a729f93960f7e2e1b3740da2ce
abeleyasu <abeleyasu98@gmail.com>
2024-08-02 11:54:37 -0700

    16-shows_by_genre.sql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,9 +1,9 @@
--- Use the hbtn_0d_usa database
-USE hbtn_0d_usa;
-
--- Query to select cities of California without using JOIN
-SELECT * FROM cities
-WHERE state_id = (
-    SELECT id FROM states WHERE name = 'California'
-)
-ORDER BY id ASC;
+-- Lists all cities of CA in the database hbtn_0d_usa.
+-- Results are ordered by ascending cities.id.
+SELECT `id`, `name`
+  FROM `cities`
+ WHERE `state_id` IN
+       (SELECT `id`
+	  FROM `states`
+	 WHERE `name` = "California")
+ ORDER BY `id`;
--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,8 +1,7 @@
--- File: 9-cities_by_state_join.sql
-
-USE hbtn_0d_usa;
-
-SELECT cities.id, cities.name AS city_name, states.name AS state_name
-FROM cities
-INNER JOIN states ON cities.state_id = states.id
-ORDER BY cities.id ASC;
+-- Lists all cities in the database hbtn_0d_usa.
+-- Records are sorted in order of ascending cities.id.
+SELECT c.`id`, c.`name`, s.`name`
+  FROM `cities` AS c
+       INNER JOIN `states` AS s
+       ON c.`state_id` = s.`id`
+ ORDER BY c.`id`;
